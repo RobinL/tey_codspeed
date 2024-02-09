a776d49e6e83f5a850f9da5fb2a6b7e40eb0bc44
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-02-09 12:35:01 +0000

    Add benchmark tests for profile_columns method

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -5,6 +5,27 @@ from splink.duckdb.linker import DuckDBLinker
 @pytest.mark.benchmark
 def test_profile(historical_50k_df):
 
+    1 + 1
+
+
+@pytest.mark.benchmark
+def test_profile_1000(historical_50k_df):
+
+    settings = {
+        "link_type": "dedupe_only",
+    }
+    linker = DuckDBLinker(historical_50k_df.head(1000), settings)
+
+    linker.profile_columns(
+        ["first_name", "postcode_fake", "substr(dob, 1,4)"], top_n=10, bottom_n=5
+    )
+
+    1 + 1
+
+
+@pytest.mark.benchmark
+def test_profile_50k(historical_50k_df):
+
     settings = {
         "link_type": "dedupe_only",
     }
@@ -13,3 +34,5 @@ def test_profile(historical_50k_df):
     linker.profile_columns(
         ["first_name", "postcode_fake", "substr(dob, 1,4)"], top_n=10, bottom_n=5
     )
+
+    1 + 1
